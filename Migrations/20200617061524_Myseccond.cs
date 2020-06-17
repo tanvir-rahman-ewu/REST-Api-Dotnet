55861e02f9482afff9ce67d21f3a0933797a57df
TanvirRahman <tanvir.rahman.ewu@gmail.com>
2020-06-17 12:18:27 +0600

    database connected

---
+++ Migrations/20200617061524_Myseccond.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Commander.Migrations
+{
+    public partial class Myseccond : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Platform",
+                table: "Commands",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Line",
+                table: "Commands",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "HowTo",
+                table: "Commands",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Platform",
+                table: "Commands",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 250);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Line",
+                table: "Commands",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 250);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "HowTo",
+                table: "Commands",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 250);
+        }
+    }
+}
